2aa9d0c7daccb82e9cfbef49932bc58632d84283
Tanner Heffner <tannerjheffner@gmail.com>
2016-08-02 01:16:07 -0700

    fix slideshow js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,35 +1,46 @@
 // Allows forward/back functionality for slideshow images.
 $(window).load(function(){
 		var pages = $('.slideshow li');
-    var current = 0;
+    var counter = 0;
 		var currentPage, nextPage;
 
-		$('.slideshow').click(function(){
-			// Set current page # equal to counter value.
-			currentPage = pages.eq(current);
+		$('.slideshow .nextButton, .slideshow >ul > li > img').click(function(){
+			// Set page # equal to counter value.
+			currentPage = pages.eq(counter);
+			currentPage.show();
+			// Up the counter
+			counter++
 
-			// If slideshow is clicked, up the counter.
-			if (current >= pages.length - 1) {
-				current = 0;
-			} else {
-				current++;
-			}
-
-			// If the back button is clicked, decrease counter.
-			if($(this).hasClass('prevButton')) {
-				// Allow looping from first to last image for ease of UX.
-				if (current <= 0) {
-					current = pages.length - 1;
-				}	else {
-					current = current - 1;
-				}
+			// Counter can't go past # of items in slideshow.
+			if (counter >= pages.length) {
+				counter = 0;
 			}
 
 			// Set nextpage to updated counter value.
-			nextPage = pages.eq(current);
+			nextPage = pages.eq(counter);
 
 			// Swap display of pages.
 			currentPage.hide();
 			nextPage.show();
 		});
+
+		// If the back button is clicked, decrease counter.
+		$('.slideshow .prevButton').click(function(){
+			currentPage = pages.eq(counter);
+
+			// Allow looping from first to last image for ease of UX.
+			if (counter <= 0) {
+				counter = pages.length - 1;
+			}	else {
+				counter = counter - 1;
+			}
+			
+			// Set nextpage to updated counter value.
+			nextPage = pages.eq(counter);
+
+			// Swap display of pages.
+			currentPage.hide();
+			nextPage.show();
+		});
+
 });
